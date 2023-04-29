af505cd16dd713e9f39acdd85bf5caf2f8911d87
Ali <alibahaari1997@gmail.com>
2023-04-30 00:10:16 +0430

    Completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -76,7 +76,7 @@ export default function Home() {
 
   return (
     <>
-      <MainAppBar />
+      {/* <MainAppBar /> */}
 
       <Box
         sx={{
--- pages/search.tsx
+++ pages/search.tsx
@@ -5,7 +5,7 @@ import SearchBar from "@/components/SearchBar";
 export default function Search() {
   return (
     <>
-      <MainAppBar />
+      {/* <MainAppBar /> */}
       <SearchBar />
       <AllProducts />
     </>
